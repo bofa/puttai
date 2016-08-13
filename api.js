18b453eb9e6f024102eee804a476c70781986c98
bofa <prosit86@gmail.com>
2016-08-13 11:24:38 +0200

    Added API file.

---
+++ api.js
@@ -0,0 +1,26 @@
+/**
+ * Putt AI API module.
+ * @module AIAPI
+ */
+
+/**
+@param {float} - Force in x direction
+@param {float} - Force in z direction
+*/
+function setAcceleration(x, z){
+
+}
+
+/**
+ * Callback for adding two numbers.
+ *
+ * @callback uppdateCallback
+ * @param {array} playrs - Array with players, you own player as first element.
+ */
+
+/**
+ * @param {uppdateCallback} - Callback function that will have the updated player. 
+ */
+function setUpdateCallback(callback){
+
+}
