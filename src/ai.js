008dc8a0b7f35d8a1ba07840af8c91778b7ea074
Per-Erik Karlsson <per-erik.karlsson@tieto.com>
2016-08-19 16:12:05 +0200

    Added the basic API.

--- src/ai.js
+++ src/ai.js
@@ -1,4 +1,4 @@
-import numeric, { sub } from 'numeric';
+import numeric, { sub, norm2, mul, prettyPrint } from 'numeric';
 
 export default class AI {
 
@@ -7,17 +7,32 @@ export default class AI {
     this.iterator = 0;
   }
 
+  /**
+   * Mapping from phone to world is [y, 0, x].
+   * @return {array} - Force vector for the game. x
+   */
   update() {
     const players = this.getPlayers();
     const positions = players.map(p => p.getPosition());
 
     this.iterator++;
 
+    const dir = sub(positions[0], positions[1]);
+    const norm = mul(-3/norm2(dir), dir);
+
     if(this.iterator%10===0) {
-      console.log('players', positions);
+      console.log('players', prettyPrint(norm));
     }
 
-    return sub(positions[0], positions[1]);
+    return AI.convertToPhone(norm);
+  }
+
+  static convertToWorld(v) {
+    return [v.y, v.z, v.x];
+  }
+
+  static convertToPhone(v) {
+    return [v[2], v[0], v[1]];
   }
 
 }
